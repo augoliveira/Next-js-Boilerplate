997ba24fbdbfa014a333160015fa8b78bea30f79
Augusto Developer <aug_oliv@hotmail.com>
2024-11-24 12:39:38 -0300

    commit

--- src/components/DemoBadge.tsx
+++ src/components/DemoBadge.tsx
@@ -4,7 +4,7 @@ export const DemoBadge = () => (
       href="https://github.com/ixartz/Next-js-Boilerplate"
     >
       <div className="rounded-md bg-gray-900 px-3 py-2 font-extrabold text-gray-100">
-        <span className="text-lime-400 font-mono">Demo of</span>
+        <span className="font-mono text-lime-400">Demo of</span>
         {` Next.js Boilerplate`}
       </div>
     </a>
--- src/components/DemoBanner.tsx
+++ src/components/DemoBanner.tsx
@@ -4,6 +4,6 @@ export const DemoBanner = () => (
   <div className="sticky top-0 z-50 bg-gray-900 p-4 text-center text-base/7 font-semibold text-gray-100 [&_a:hover]:text-indigo-500 [&_a]:text-green-300">
     Live Demo of Next.js Boilerplate -
     {' '}
-    <Link href="/sign-up" className='text-2xl font-extrabold'>Explore the Authentication</Link>
+    <Link href="/sign-up" className="text-2xl font-extrabold">Explore the Authentication</Link>
   </div>
 );
