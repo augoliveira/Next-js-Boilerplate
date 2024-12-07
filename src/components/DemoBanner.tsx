4e46a5d8704ab2ec92f518c8773dad1e93b7e57b
Augusto Developer <aug_oliv@hotmail.com>
2024-12-07 10:16:04 -0300

    commit

--- src/components/DemoBanner.tsx
+++ src/components/DemoBanner.tsx
@@ -1,4 +1,4 @@
-import { XMarkIcon } from '@heroicons/react/20/solid'
+import { XMarkIcon } from '@heroicons/react/24/solid'
 
 export const DemoBanner = () => (
   <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
